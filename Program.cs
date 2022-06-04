20959ec8292241224301dc4633d756e645bd3592
IRBIS <IRBIS@IRBIS-NB144>
2022-06-04 16:08:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 353;
+
+            int summ = a + b;
+            Console.WriteLine(summ);
+            Console.WriteLine(a);
+        }
+    }
+}
